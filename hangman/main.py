9cbea491a35a755d4d4f7bfbeb8fa6388dc13d87
Uday <Chahal.Uday@gobindsarvar.ca>
2026-03-17 11:20:33 -0700

    hangman

---
+++ hangman/main.py
@@ -0,0 +1,84 @@
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel"]
+lives = 6
+chosen_word = random.choice(word_list)
+correct_letters = []
+is_game_over = False
+while not is_game_over:
+    guess = input("Make your guess: ").lower()
+    display = ""
+    if guess not in chosen_word:
+        lives -= 1
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    if lives == 0:
+        is_game_over = True
+        print("you loose!")
+
+    print(display)
+    print(stages[::-1][lives])
+    print("lives: ", lives)
